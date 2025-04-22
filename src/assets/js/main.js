7d64812fab1bd20c9dc3c28f80705d65362c5696
mr-stubblefield <mr.stubblefield@rvrvw.org>
2025-04-22 18:56:17 +0000

    made search history better

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -59,6 +59,11 @@ function useHistory(verse) {
     verseLookup();
 }
 
+function clearSearchHistory() {
+    document.getElementById('history').innerHTML = '';
+    searchHistory.clear();
+}
+
 async function verseLookup() {
     var verse = document.getElementById("search").value;
     var headings = document.getElementById("headings").checked;
@@ -84,4 +89,45 @@ inputField.addEventListener('keydown', function(event) {
 
 ["headings", "extras", "numbers"].forEach(function(id) {
     document.getElementById(id).addEventListener('change', verseLookup);
+});
+
+
+
+
+function hasClass(ele, cls) {
+    return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) ele.className += " " + cls;
+}
+
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
+
+//Add event from js the keep the marup clean
+function init() {
+    document.getElementById("open-menu").addEventListener("click", toggleMenu);
+    document.getElementById("body-overlay").addEventListener("click", toggleMenu);
+}
+
+//The actual fuction
+function toggleMenu() {
+    var ele = document.getElementsByTagName('body')[0];
+    if (!hasClass(ele, "menu-open")) {
+        addClass(ele, "menu-open");
+    } else {
+        removeClass(ele, "menu-open");
+    }
+}
+
+//Prevent the function to run before the document is loaded
+document.addEventListener('readystatechange', function() {
+    if (document.readyState === "complete") {
+        init();
+    }
 });
