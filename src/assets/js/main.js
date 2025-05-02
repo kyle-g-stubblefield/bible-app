769667a3de1d679cc64aeb4e1a6778162af3c3dc
mr-stubblefield <mr.stubblefield@rvrvw.org>
2025-05-02 18:51:45 +0000

    fixed darkmode toggle

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,8 +5,10 @@ var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     themeToggleLightIcon.classList.remove('hidden');
+    document.getElementById("theme-toggle").checked = true;
 } else {
     themeToggleDarkIcon.classList.remove('hidden');
+    document.getElementById("theme-toggle").checked = false;
 }
 
 var themeToggleBtn = document.getElementById('theme-toggle');
