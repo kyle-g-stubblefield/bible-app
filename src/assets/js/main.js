fc4d440b1c32e875b38823834c76515cd9afa5bb
Kyle Stubblefield <kyle.g.stubblefield@gmail.com>
2025-05-27 18:57:35 +0000

    romans road in the right order

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -123,23 +123,18 @@ async function verseLookup() {
             "Romans 10:17"
         ];
         let collector = "";
-        let count = verses.length;
-        verses.forEach(verse => {
-            let url = "/api?verse=" + verse + "&headings=" + headings + "&extras=" + extras + "&numbers=" + numbers;
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                collector += data.passages.join("");
-                
-                //searchHistory.add(data.query);
-                //    createHistory();
-                //    wrapText();
+        getDataInOrder(verses).then(data => {
+            let count = data.length;
+            data.forEach(obj => {
+                collector += obj.passages.join("");
                 if (--count === 0) {
-                    document.getElementById("verse").innerHTML = collector;
+                    document.getElementById("verse").innerHTML += collector;
+                    searchHistory.add("Romans Road");
+                    createHistory();
+                    wrapText();
                 }
             });
         });
-
     } else {
         var url = "/search?search=" + verse;
         fetch(url)
@@ -158,6 +153,16 @@ async function verseLookup() {
     }
     window.scrollTo(0,0);
 }
+
+
+async function getDataInOrder(verses) {
+    const promises = verses.map(verse => {
+        let url = "/api?verse=" + verse + "&headings=false&extras=false&numbers=false";
+        return fetch(url).then(response => response.json());
+    });
+    return await Promise.all(promises);
+}
+
 
 var inputField = document.getElementById('search');
 inputField.addEventListener('keydown', function(event) {
